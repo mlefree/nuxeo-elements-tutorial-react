5a36d35b5075edfa7c4f05b16216ece31f2e6f6c
mlefree <>
2019-05-23 08:42:45 +0200

    nuxeo react

---
+++ README.md
@@ -0,0 +1,53 @@
+# Nuxeo Elements tutorial for React.js
+
+Our [Nuxeo Elements tutorial](https://doc.nuxeo.com/nxdoc/nuxeo-elements-tutorial/) for [React.js](https://reactjs.org/)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm start
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Test
+```
+npm test
+```
+
+
+### Advanced commands
+
+```
+npm run eject
+```
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+
+Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+
+
+## License
+
+[Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html) 
+
+(C) Copyright Nuxeo Corp. (http://nuxeo.com/)
+
+All images, icons, fonts, and videos contained in this folder are copyrighted by Nuxeo, all rights reserved.
+
+## About Nuxeo
+
+Nuxeo dramatically improves how content-based applications are built, managed and deployed, making customers more agile, innovative and successful. Nuxeo provides a next generation, enterprise ready platform for building traditional and cutting-edge content oriented applications. Combining a powerful application development environment with SaaS-based tools and a modular architecture, the Nuxeo Platform and Products provide clear business value to some of the most recognizable brands including Verizon, Electronic Arts, Sharp, FICO, the U.S. Navy, and Boeing. Nuxeo is headquartered in New York and Paris. More information is available at www.nuxeo.com.
+
+
---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+
+import '@nuxeo/nuxeo-elements/nuxeo-connection'
+import '@nuxeo/nuxeo-elements/nuxeo-document'
+import '@nuxeo/nuxeo-elements/nuxeo-page-provider'
+import '@nuxeo/nuxeo-ui-elements/widgets/nuxeo-file'
+import '@nuxeo/nuxeo-ui-elements/widgets/nuxeo-document-suggestion'
+import './nuxeo-documents-table'
+
+import {RoutingBehavior} from '@nuxeo/nuxeo-ui-elements/nuxeo-routing-behavior';
+// define legacy router for elements using `nuxeo-router-behavior`
+RoutingBehavior.__router = {
+    browse() {
+        return '#';
+    },
+};
+
+class AppComponent extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.currentRef = null;
+        this.currentDoc = null;
+        this.currentPath = null;
+
+        this.setRef = element => {
+            this.currentRef = element;
+        };
+
+        this.focusCurrentRef = () => {
+            if (this.currentRef) {
+                this.currentRef.focus();
+                this.currentRef.addEventListener('selected-item-changed', (e) => {
+                    this.selectDoc(e);
+                });
+            }
+        };
+
+        this.selectDoc = (e) => {
+            let element = <h2>&nbsp;</h2>;
+            if (e && e.detail && e.detail.value) {
+                this.currentDoc = e.detail.value;
+                this.currentPath = e.detail.value.path;
+                const contributors = this.currentDoc.properties['dc:contributors'];
+                element = (
+                    <div>
+                        <h2>Title: {this.currentDoc.title}</h2>
+                        <p>ID: {this.currentDoc.uid}</p>
+                        <p>Repository: {this.currentDoc.repository}</p>
+                        <p>State: {this.currentDoc.state}</p>
+                        <div>{contributors}</div>
+                    </div>);
+            } else {
+                this.currentPath = this.currentDoc = undefined;
+            }
+
+            ReactDOM.render(element, document.getElementById('selectedDoc'));
+        }
+    }
+
+    componentDidMount() {
+        this.focusCurrentRef();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                </header>
+
+                <nuxeo-connection url="http://localhost:8110/nuxeo"></nuxeo-connection>
+
+                <nuxeo-document-suggestion
+                    placeholder="Select a document"
+                    ref={this.setRef}>
+                </nuxeo-document-suggestion>
+
+                <nuxeo-document
+                    auto
+                    doc-path="{currentPath}"></nuxeo-document>
+
+                <div id="selectedDoc"></div>
+
+            </div>
+        );
+    }
+}
+
+export default AppComponent;
