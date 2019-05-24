0c6c17d5f46e337705cd2b480268ef291c0da9f4
Nelson Silva <nsilva@nuxeo.com>
2019-05-24 15:52:56 +0100

    Simplify things a bit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+// define legacy router for elements using `nuxeo-router-behavior`
+import {RoutingBehavior} from '@nuxeo/nuxeo-ui-elements/nuxeo-routing-behavior';
+RoutingBehavior.__router = {
+    browse() {
+        return '#';
+    },
+};
+
 ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
